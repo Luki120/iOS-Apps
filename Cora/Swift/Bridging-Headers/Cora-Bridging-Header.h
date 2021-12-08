bd66d64211fc8f1f84d10ea8f81d0a7305982a61
luki120 <luki120@exerra.xyz>
2021-12-08 04:55:06 -0300

    .

--- Cora/Swift/Bridging-Headers/Cora-Bridging-Header.h
+++
@@ -1,1 +0,0 @@
-#import "NSTask.h"
--- Cora/Swift/Bridging-Headers/NSTask.h
+++
@@ -1,9 +0,0 @@
-@import Foundation;
-
-
-@interface NSTask : NSObject
-@property (strong) id standardOutput;
-@property (copy) NSString *launchPath;
-@property (copy) NSArray<NSString *> *arguments;
-- (void)launch;
-@end
