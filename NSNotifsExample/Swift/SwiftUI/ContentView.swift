6e71133f768005cc9a44e53187c9f49d7d4a8b52
luki120 <luki120@exerra.xyz>
2021-12-08 18:15:42 -0300

    fixed indentation consistency

--- NSNotifsExample/Swift/SwiftUI/ContentView.swift
+++ NSNotifsExample/Swift/SwiftUI/ContentView.swift
@@ -8,19 +8,17 @@ struct ContentView: View {
 	because first I didn't know theos supported it and also
 	because these property wrappers take care of all the work :TimPog ---*/
 
-	@AppStorage("counterCount") private var counter = 0
 	@State private var color:Color = .randomColor
 	@State private var labelAlpha = 0.0
+
+	@AppStorage("counterCount") private var counter = 0
 
 	private let firstColor = Color(red: 0.74, green: 0.78, blue: 0.98)
 	private let secondColor = Color(red: 0.77, green: 0.69, blue: 0.91)
 
 	init() {
-
-		UITabBar.appearance().tintColor = .systemPurple
 		UITabBar.appearance().barTintColor = .systemBackground
 		UITabBar.appearance().isTranslucent = false
-
 	}
 
 	var body: some View {
@@ -43,55 +41,52 @@ struct ContentView: View {
 					})
 				}
 			}
-			.font(.system(size :14))
-			.frame(width: 260, height: 40)
-			.buttonStyle(StaticButtonStyle())
-			.background(Color(.systemIndigo))
-			.foregroundColor(.white)
-			.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-			.multilineTextAlignment(.center)
+				.font(.system(size :14))
+				.frame(width: 260, height: 40)
+				.buttonStyle(StaticButtonStyle())
+				.background(Color(.systemIndigo))
+				.foregroundColor(.white)
+				.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+				.multilineTextAlignment(.center)
 
-			.tabItem {
-				Image(systemName: "bolt.horizontal.fill")
-				Text("Home")
-			}
+				.tabItem {
+					Image(systemName: "bolt.horizontal.fill")
+					Text("Home")
+				}
 
 			VStack {
 
 				Spacer()
 
 				Button(String(counter)) {}
-				.frame(width: 150, height: 40)
-				.background(color)
-				.foregroundColor(.white)
-				.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+					.frame(width: 150, height: 40)
+					.background(color)
+					.foregroundColor(.white)
+					.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
 
-				Button("Reset counter") {
-					counter = 0
-				}
-				.frame(width: 150, height: 40)
-				.background(LinearGradient(
-					gradient: 
-					Gradient(colors: [firstColor, secondColor]),
-					startPoint: .leading, 
-					endPoint: .trailing)
-				)
-				.foregroundColor(.white)
-				.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-				.padding(.top, 10)
+				Button("Reset counter") { counter = 0 }
+					.frame(width: 150, height: 40)
+					.background(LinearGradient(
+						gradient: 
+						Gradient(colors: [firstColor, secondColor]),
+						startPoint: .leading, 
+						endPoint: .trailing)
+					)
+					.foregroundColor(.white)
+					.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+					.padding(.top, 10)
 
 				Spacer()
 
 				Text("The power of Notifications :fr: Learn them, embrace them, and use them to make awesome stuff")
-				.font(.system(size: 18))
-				.opacity(labelAlpha)
-				.padding(.all, 20)
-				.lineLimit(nil)
-				.foregroundColor(Color(.systemPurple))
-				.multilineTextAlignment(.center)
+					.font(.system(size: 18))
+					.opacity(labelAlpha)
+					.padding(.all, 20)
+					.lineLimit(nil)
+					.foregroundColor(Color(.systemPurple))
+					.multilineTextAlignment(.center)
 
 			}
-
 			.tabItem {
 				Image(systemName: "bonjour")
 				Text("Not Home")
