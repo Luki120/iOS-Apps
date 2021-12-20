69266c30733d2c3bd6da08241fe589827fcb957d
luki120 <luki120@exerra.xyz>
2021-12-20 01:20:40 -0300

    Custom modifiers file

---
+++ Aurora/SwiftUI/CustomModifiers/CustomModifiers.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+
+struct ButtonStyle: ViewModifier {
+
+	private let auroraColor = Color(red: 0.74, green: 0.78, blue: 0.98)
+
+	func body(content: Content) -> some View {
+
+		content
+			.font(.system(size: 18))
+			.frame(width: 220, height: 44)
+			.background(auroraColor)
+			.foregroundColor(.white)
+			.clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+
+	}
+
+}
+
+
+struct LabelStyle: ViewModifier {
+
+	func body(content: Content) -> some View {
+
+		content
+			.font(.system(size: 22))
+			.lineLimit(1)
+			.minimumScaleFactor(0.5)
+			.transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.5)))
+
+	}
+
+}
