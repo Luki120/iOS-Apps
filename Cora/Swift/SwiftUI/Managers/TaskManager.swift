10ad846338c7a08e2bf1a95b387d5ce867dca626
luki120 <luki120@exerra.xyz>
2022-01-13 05:55:03 -0300

    improved toggle reliability + no need for singleton

--- Cora/Swift/SwiftUI/Managers/TaskManager.swift
+++ Cora/Swift/SwiftUI/Managers/TaskManager.swift
@@ -4,40 +4,40 @@ import SwiftUI
 
 final class TaskManager: ObservableObject {
 
-	static let sharedInstance = TaskManager()
+	@AppStorage("switchState") var shouldPrintDarwinInformation = false
 
 	@Published var darwinString: String?
 	@Published var uptimeString: String?
-	
-	init() {}
-	
-	func launchDarwinTask() {
+
+	init() {
+		launchTask()
+	}
+
+	func launchTask() {
+
+		let task = NSTask()
+		task.arguments = ["-c", "uptime"]
+		task.launchPath = "/bin/sh"
+
+		let pipe = Pipe()
+		task.standardOutput = pipe
+		task.launch()
+
+		let data = pipe.fileHandleForReading.readDataToEndOfFile()
+		uptimeString = String(data: data, encoding: .utf8)
+
+		guard shouldPrintDarwinInformation else { return }
 
 		let darwinTask = NSTask()
 		darwinTask.arguments = ["-c", "uname -a"]
 		darwinTask.launchPath = "/bin/sh"
-		
-		let pipe = Pipe()
-		darwinTask.standardOutput = pipe
+
+		let darwinPipe = Pipe()
+		darwinTask.standardOutput = darwinPipe
 		darwinTask.launch()
-		
-		let data = pipe.fileHandleForReading.readDataToEndOfFile()
-		darwinString = String(data: data, encoding: .utf8)
 
-	}
-
-	func launchUptimeTask() {
-
-		let uptimeTask = NSTask()
-		uptimeTask.arguments = ["-c", "uptime"]
-		uptimeTask.launchPath = "/bin/sh"
-		
-		let pipe = Pipe()
-		uptimeTask.standardOutput = pipe
-		uptimeTask.launch()
-		
-		let data = pipe.fileHandleForReading.readDataToEndOfFile()
-		uptimeString = String(data: data, encoding: .utf8)
+		let darwinData = darwinPipe.fileHandleForReading.readDataToEndOfFile()
+		darwinString = String(data: darwinData, encoding: .utf8)
 
 	}
 
