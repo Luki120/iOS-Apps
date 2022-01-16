cf3a621687db01e8f41ca211455f58997b3c4951
luki120 <luki120@exerra.xyz>
2022-01-16 18:30:10 -0300

    removed code duplication

--- Cora/Swift/UIKit/Managers/TaskManager.swift
+++ Cora/Swift/UIKit/Managers/TaskManager.swift
@@ -5,48 +5,22 @@ final class TaskManager {
 
 	static let sharedInstance = TaskManager()
 
-	var darwinString: String?
-	var uptimeString: String?
+	var outputString: String?
 
 	private init() {}
 
-	func launchDarwinTask() {
-
-		UserDefaultsManager.sharedInstance.loadSwitchState()
-
-		guard let switchState = UserDefaultsManager.sharedInstance.switchState else {
-			return
-		}
-
-		if !switchState {
-			return
-		}
-
-		let darwinTask = NSTask()
-		darwinTask.arguments = ["-c", "uname -a"]
-		darwinTask.launchPath = "/bin/sh"
+	func launchTask(withArguments arguments: [String]) {
 
 		let pipe = Pipe()
-		darwinTask.standardOutput = pipe
-		darwinTask.launch()
+		let task = NSTask()
+
+		task.arguments = arguments
+		task.launchPath = "/bin/sh"
+		task.standardOutput = pipe
+		task.launch()
 
 		let data = pipe.fileHandleForReading.readDataToEndOfFile()
-		darwinString = String(data: data, encoding: .utf8)
-
-	}
-
-	func launchUptimeTask() {
-
-		let uptimeTask = NSTask()
-		uptimeTask.arguments = ["-c", "uptime"]
-		uptimeTask.launchPath = "/bin/sh"
-
-		let pipe = Pipe()
-		uptimeTask.standardOutput = pipe
-		uptimeTask.launch()
-
-		let data = pipe.fileHandleForReading.readDataToEndOfFile()
-		uptimeString = String(data: data, encoding: .utf8)
+		outputString = String(data: data, encoding: .utf8)
 
 	}
 
