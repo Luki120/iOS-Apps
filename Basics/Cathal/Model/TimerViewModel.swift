ebc1abdb009b7d3fa48537c7c79e576eeb031d13
luki120 <luki120@exerra.xyz>
2022-08-06 20:35:05 -0300

    not needed

--- Basics/Cathal/Model/TimerViewModel.swift
+++ Basics/Cathal/Model/TimerViewModel.swift
@@ -11,14 +11,13 @@ final class TimerViewModel: ObservableObject {
 	@Published var minutes = 0
 	@Published var seconds = 0
 	@Published var breakMinutes = 0
-	@Published var breakSeconds = 0
 	@Published var totalSeconds = 0
 
 	@Published private(set) var timerString = "00:00"
 	@Published private(set) var totalStaticSeconds = 0
 
-	func startTimer() { startTimerWith(minutes, seconds, passingFlag: &isActive) }
-	func startBreakTimer() { startTimerWith(breakMinutes, breakSeconds, passingFlag: &isBreakActive) }
+	func startTimer() { startTimerWith(minutes, passingFlag: &isActive) }
+	func startBreakTimer() { startTimerWith(breakMinutes, passingFlag: &isBreakActive) }
 
 	func stopTimer() {
 		withAnimation {
@@ -27,7 +26,6 @@ final class TimerViewModel: ObservableObject {
 			minutes = 0
 			seconds = 0
 			breakMinutes = 0
-			breakSeconds = 0
 			progress = 1
 		}
 		totalSeconds = 0
@@ -36,33 +34,33 @@ final class TimerViewModel: ObservableObject {
 	}
 
 	func updateTimer() {
-		updateTimerWith(&minutes, &seconds, isInBreak: false)
+		updateTimerWith(&minutes, isInBreak: false)
 		guard minutes == 0 && seconds == 0 else { return }
 		isActive = false
-		startTimerWith(breakMinutes, breakSeconds, passingFlag: &isBreakActive)
+		startTimerWith(breakMinutes, passingFlag: &isBreakActive)
 	}
 
-	func updateBreakTimer() { updateTimerWith(&breakMinutes, &breakSeconds, isInBreak: true) }
+	func updateBreakTimer() { updateTimerWith(&breakMinutes, isInBreak: true) }
 
-	private func startTimerWith(_ min: Int, _ secs: Int, passingFlag flag: inout Bool) {
+	private func startTimerWith(_ min: Int, passingFlag flag: inout Bool) {
 		withAnimation(.easeInOut(duration: 1.0)) { flag = true }
-		timerString = "\(min):\(secs < 10 ? "0" : "")\(secs)"
+		timerString = "\(min):\(seconds < 10 ? "0" : "")\(seconds)"
 
-		totalSeconds = (min * 60) + secs
+		totalSeconds = (min * 60) + seconds
 		totalStaticSeconds = totalSeconds
 		startNewTimer = false
 	}
 
-	private func updateTimerWith(_ min: inout Int, _ secs: inout Int, isInBreak: Bool) {
+	private func updateTimerWith(_ min: inout Int, isInBreak: Bool) {
 		progress = CGFloat(totalSeconds) / CGFloat(totalStaticSeconds)
 		progress = progress < 0 ? 0 : progress
 		totalSeconds -= 1
 		min = (totalSeconds / 60) % 60
-		secs = totalSeconds % 60
-		timerString = "\(min):\(secs < 10 ? "0" : "")\(secs)"
+		seconds = totalSeconds % 60
+		timerString = "\(min):\(seconds < 10 ? "0" : "")\(seconds)"
 
 		guard isInBreak else { return }
- 		guard min == 0 && secs == 0 else { return }
+ 		guard min == 0 && seconds == 0 else { return }
 
 		isActive = false
 		isBreakActive = false
